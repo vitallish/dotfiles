5b4e02d0e18df4357ea05f9f1a26755dfb32d406
Vitaly Druker <vitaly@westendstats.com>
2023-12-27 01:16:41 -0500

    made it so clicking on media brought you to player -dropped app from text

--- .config/sketchybar/plugins/media.sh
+++ .config/sketchybar/plugins/media.sh
@@ -2,8 +2,9 @@
 STATE="$(echo "$INFO" | jq -r '.state')"
 
 if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.app + ": " + .title + " - " + .artist')"
-  sketchybar --set $NAME label="$MEDIA" drawing=on
+  APP="$(echo "$INFO" | jq -r '.app')"
+  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
+  sketchybar --set $NAME label="$MEDIA" drawing=on click_script="osascript -e 'tell application \"$APP\" to activate'"
 else
   sketchybar --set $NAME drawing=off
 fi
