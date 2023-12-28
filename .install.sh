7a3231331fdc41be2539890bf374ec9fbe3ea540
Vitaly Druker <vitaly@westendstats.com>
2023-12-28 08:54:20 -0500

    renamed install script

--- .install.sh
+++ .install.sh
@@ -1,3 +1,7 @@
 
 [ ! -d "$HOME/.cfg" ] && git clone --bare git@github.com:vitallish/dotfiles.git $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout main
+
+source $HOME/.zshrc
+config config --local status.showUntrackedFiles no
+
