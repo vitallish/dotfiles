9e7f340b0c41c268f071b6b25aaea5f59b1ad8ab
Vitaly Druker <vitaly@westendstats.com>
2024-02-14 16:07:20 -0500

    updates

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,12 +14,31 @@ return {
         end
   },
   {
+    'godlygeek/tabular', 
+    -- Tabularize command is required by vim-markdown
+    -- however it doesn't work onless tabular is loaded alread
+    -- I think this needs to be done by loading this 
+    -- in the config file or something?
+     lazy = false
+  },
+  {
   'preservim/vim-markdown',
+  -- for some reason plugin does not automatically load with filetim
+  ft = "markdown",
+  config = function()
+	  -- https://github.com/preservim/vim-markdown?tab=readme-ov-file#options
+	vim.cmd([[let g:vim_markdown_folding_disabled = 0]])
+	vim.cmd([[let g:vim_markdown_folding_level = 2]])
+	vim.cmd([[let g:vim_markdown_math = 1]])
+	end,
+
   branch = 'master',
   require = {'godlygeek/tabular'}
-},{
-  'chrisbra/csv.vim',
-  branch = 'master'}
+},
+--{
+  --'chrisbra/csv.vim',
+  --lazy = false,
+  --branch = 'master'}
 
 }
 
