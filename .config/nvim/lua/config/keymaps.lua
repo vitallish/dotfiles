6c1ca112e76abdcb081bf3d9105fc6ab4ca98bba
Vitaly Druker <vitaly@westendstats.com>
2024-02-22 22:20:52 -0500

    added some more options, keymaps

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- https://medium.com/@shaikzahid0713/keyboard-shortcuts-in-neovim-d04cd7f551a7
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
+-- navigate buffers
+keymap("n", "<tab>", ":bnext<cr>", opts) -- Next Tab 
+keymap("n", "<s-tab>", ":bprevious<cr>", opts) -- Previous tab
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,4 +1,18 @@
 local indent = 2
 
-vim.opt.number = true
+local opt = vim.opt
 
+opt.number = true
+
+-- https://medium.com/@shaikzahid0713/2bc6b878682
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = indent -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = indent -- Number of spaces tabs count for
+
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
