85385693a70679d9d87b74d8fa83aa89e45ccf22
Vitaly Druker <vdruker@gmail.com>
2024-03-05 11:23:55 -0500

    added some customization for notesand disabling tmux

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -2,14 +2,13 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  --ft = "markdown",
+   event = {
+     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+     "BufReadPre " .. vim.fn.expand "~" .. "/Notes/personal/**.md",
+     "BufNewFile " .. vim.fn.expand "~" .. "/Notes/personal/**.md",
+   },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
@@ -41,6 +40,13 @@ return {
           path = "~/Notes/personal",
         },
       },
+      daily_notes = {
+        folder = "daily",
+        template = "Daily Template.md",
+      },
+      templates = {
+        subdir = "templates",
+      }
 
     }
 
