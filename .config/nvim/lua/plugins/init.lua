f5cd46f727f66ac950e1be991a6655cd3209d8f9
Vitaly Druker <vitaly@westendstats.com>
2024-03-14 10:36:27 -0400

    added outline

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -48,5 +48,14 @@ return {
       })
     end
   },
+  {
+  'stevearc/aerial.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "nvim-tree/nvim-web-devicons"
+  },
+}
 }
 
