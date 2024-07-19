8aae1717e873a19fa40321900885ff8ee5f6044c
Vitaly Druker <vitaly@westendstats.com>
2024-07-19 16:05:11 -0400

    update apt and flatpaks

--- .update_apps.sh
+++ .update_apps.sh
@@ -1,2 +1,13 @@
 
+pkill beeper
 sudo curl -o /opt/beeper/beeper https://download.beeper.com/linux/appImage/x64
+/opt/beeper/beeper &
+disown %1
+
+
+sudo apt update &&
+sudo apt full-upgrade -y &&
+sudo apt autoremove -y &&
+sudo apt autoclean -y &&
+flatpak update -y
+
