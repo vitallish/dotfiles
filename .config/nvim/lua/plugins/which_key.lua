3644affe2e5640a89015a222935b4bf0fc4599fa
Vitaly Druker <vdruker@gmail.com>
2024-11-18 10:15:38 -0500

    removed old commented code

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -15,34 +15,8 @@ return {
       
 
     },
-    -- config = function()
-    --   local wk = require("which-key")
-    --   -- As an example, we will create the following mappings:
-    --   --  * <leader>ff find files
-    --   --  * <leader>fr show recent files
-    --   --  * <leader>fb Foobar
-    --   -- we'll document:
-    --   --  * <leader>fn new file
-    --   --  * <leader>fe edit file
-    --   -- and hide <leader>1
-    --
-    --   wk.register({
-    --     f = {
-    --       name = "file", -- optional group name
-    --       s = { 
-    --         name = "Scratch", 
-    --         e = { "<cmd>edit ~/Notes/personal/zzz/Scratch.md<cr>", "Open"},
-    --         v = { "<cmd>vsplit ~/Notes/personal/zzz/Scratch.md<cr>", "Open vertical split"},
-    --         h = { "<cmd>split ~/Notes/personal/zzz/Scratch.md<cr>", "Open vertical split"},
-    --       },
-    --     },
-    --   }, { prefix = "<leader>" })
-    -- end, 
     opts = {
       notify = false,
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     }
   }
 }
