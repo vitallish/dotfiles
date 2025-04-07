a98e97ecc61c9eac51fa4e675c597d0eec7eb056
Vitaly Druker <vdruker@gmail.com>
2025-04-07 16:55:09 -0400

    added firefox launching to qutebrowser

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,3 +1,5 @@
 config.load_autoconfig()
 
 config.bind(',p', 'open https://ccny-proxy1.libr.ccny.cuny.edu/login?qurl={url:pretty}')
+config.bind(',f', 'spawn --detach firefox --new-window "{url}"')
+
