e867211b6262e9488446982beabc15d93ca64395
Vitaly Druker <vdruker@gmail.com>
2025-06-05 15:50:37 -0400

    misc

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -3,3 +3,27 @@ config.load_autoconfig()
 config.bind(',p', 'open https://ccny-proxy1.libr.ccny.cuny.edu/login?qurl={url:pretty}')
 config.bind(',f', 'spawn --detach firefox --new-window "{url}"')
 
+## How to open links in an existing instance if a new one is launched.
+## This happens when e.g. opening a link from a terminal. See
+## `new_instance_open_target_window` to customize in which window the
+## link is opened in.
+## Type: String
+## Valid values:
+##   - tab: Open a new tab in the existing window and activate the window.
+##   - tab-bg: Open a new background tab in the existing window and activate the window.
+##   - tab-silent: Open a new tab in the existing window without activating the window.
+##   - tab-bg-silent: Open a new background tab in the existing window without activating the window.
+##   - window: Open in a new window.
+##   - private-window: Open in a new private window.
+c.new_instance_open_target = 'tab'
+
+## Which window to choose when opening links as new tabs. When
+## `new_instance_open_target` is set to `window`, this is ignored.
+## Type: String
+## Valid values:
+##   - first-opened: Open new tabs in the first (oldest) opened window.
+##   - last-opened: Open new tabs in the last (newest) opened window.
+##   - last-focused: Open new tabs in the most recently focused window.
+##   - last-visible: Open new tabs in the most recently visible window.
+c.new_instance_open_target_window = 'last-focused'
+
