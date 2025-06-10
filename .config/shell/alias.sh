5ac70d7a376b8632efdbe3414c96c59b32da590d
Vitaly Druker <vdruker@gmail.com>
2025-06-09 21:49:15 -0400

    many changes will be better now that i have lazygit

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -9,7 +9,7 @@ alias qb="$SCRIPTS/qbprof.sh"
 # For use with dotfile config
 # https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-
+alias lazyconfig='lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 # Temp workaround to disable punycode deprecation logging to stderr
 # https://github.com/bitwarden/clients/issues/6689
 alias bw='NODE_OPTIONS="--no-deprecation" bw'
