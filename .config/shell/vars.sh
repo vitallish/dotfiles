6a637d227b8ff62b06670d899ea878dd562860be
Vitaly Druker <vdruker@gmail.com>
2025-09-08 22:20:09 -0400

    fixed up a bunch of stuff with tasks/time

--- .config/shell/vars.sh
+++ .config/shell/vars.sh
@@ -13,6 +13,11 @@ if [[ "$VAKD_COMP_OWNER" == "AZ" ]]; then
   export HOMEBREW_BUNDLE_FILE_GLOBAL="$HOMEBREW_BUNDLE_FILE_GLOBAL.az"
 fi
 
+if command -v bat >/dev/null 2>&1
+then
+  # use bat for man
+  export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'"
+fi
 
 export SCRIPTS="$XDG_CONFIG_HOME/scripts/"
 export VAKD_VENVS="$XDG_DATA_HOME/venvs"
